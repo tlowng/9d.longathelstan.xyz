612f36bf9c5a281cb1046d0550650e98521ee6a7
LongAthelstan <121792617+longathelstan@users.noreply.github.com>
2024-08-29 19:03:43 +0700

    idk
    van rat la iu haanhh do nha

--- src/components/members.js
+++ src/components/members.js
@@ -146,7 +146,7 @@ const Members = (props) => {
               </div>
             </a>
             <a
-              href="https://www.facebook.com/profile.php?id=100087801795292"
+              href="/"
               target="_blank"
               rel="noreferrer noopener"
               className="members-link11"
